e1b75ef858c349c93c88dabac85ee84a364a9a4e
Duncan Lowrie <dunc@duncster.co.uk>
2015-12-16 15:47:38 +0000

    Initial commit.

--- TestBannerAutoLayout/ViewController.swift
+++ TestBannerAutoLayout/ViewController.swift
@@ -9,15 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bannerContainer: UIView!
+    @IBOutlet weak var contentContainer: UIView!
+    @IBOutlet weak var bannerhHeightConstraint: NSLayoutConstraint!
+    
+    var originalBannerHeight: CGFloat = 0.0
+    var firstRun = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.addTapRecognizer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addTapRecognizer(){
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("tapAction:")))
+    }
+    
+    func tapAction(sender: AnyObject){
+        if firstRun {
+            self.originalBannerHeight = self.bannerContainer.frame.height
+            firstRun = false
+        }
+        
+        //toggle heights...
+        var newBannerHeight: CGFloat = 0.0
+        if self.bannerhHeightConstraint.constant == 0.0 {
+            newBannerHeight = self.originalBannerHeight
+        }
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.bannerhHeightConstraint.constant =     newBannerHeight
+            self.view.layoutIfNeeded()
+        }
+        
     }
 
 
